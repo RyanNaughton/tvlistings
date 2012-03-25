4de646c124dd8aedfbb49aad8ba82dd576d00975
Ryan Naughton <Ryan.J.Naughton@gmail.com>
2012-03-25 16:48:27 -0400

    deployment config modifications

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:ryannaughton/#{application}.git"
+set :repository, "git@github.com:RyanNaughton/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
